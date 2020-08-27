63348118a16d9a5c3dc5ba5a583648d23fde9449
MostaphaLkhadir <lkhadir.mostapha@gmail.com>
2020-08-27 15:42:49 +0200

    first commit to master

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'masterApp',
+      title: 'masterApp1',
       theme: ThemeData(
         // This is the theme of your application.
         //
